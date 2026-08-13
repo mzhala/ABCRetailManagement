820e93828db982eca0e8221591150888459c7b05
HalaMzobe <HalaMzobe@HALAMZ>
2026-08-13 11:43:30 +0200

    Add project files.

---
+++ ABCRetailManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABCRetailManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
